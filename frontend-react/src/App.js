1ebf8b51a57364d9a822216df71aa386e6c80ba5
Siah Peih Wee <puaywee@gmail.com>
2023-06-28 13:36:37 +0800

    Using React Component and Router in Frontend #6

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
+
+import NavBar from './components/NavBar';
+import HomeScreen from './screens/HomeScreen';
+import ProductsScreen from './screens/ProductsScreen';
+import NoScreen from './screens/NoScreen';
+import FooterBar from './components/FooterBar';
+import SingleProductScreen from './screens/SingleProductScreen';
+
+const ProductIdWrapper = () => {
+  const { productId } = useParams();
+  return <SingleProductScreen productId={productId} />;
+};
+
+class App extends React.Component
+{
+    constructor() {
+        super()
+    }
+
+    render()
+    {
+        return (
+            <div>
+                <NavBar/>
+
+                <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductsScreen/>}/>
+                    <Route path="/products/:productId" element={<ProductIdWrapper/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+                
+                <FooterBar/>
+            </div>
+        );
+    }
 }
 
 export default App;
